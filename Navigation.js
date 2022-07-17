94eea8ae72053b90abd66da128c21a07122a2e23
Adedoyin <isradeoluwa@gmail.com>
2022-07-18 00:50:18 +0100

    Navigations

--- Navigation.js
+++ Navigation.js
@@ -1,17 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import {} from 'react-native'
+import { } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ManageExpenses from './screens/ManageExpenses';
+import AllExpenses from './screens/AllExpenses';
+import RecentExpenses from './screens/RecentExpenses';
 
-export default function NavigationType(){
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+export default function Navigations(){
     return (
         <>
             <StatusBar style="auto"/>
             <NavigationContainer>
                 <Stack.Navigator>
-                    <Stack.Screen/> 
-                    <Stack.Screen/>
+                    <Stack.Screen component={ExpensesOverview} name="ExpensesOverview"/>
+                    <Stack.Screen component={ManageExpenses} name="ManageExpenses"/>
                 </Stack.Navigator>
             </NavigationContainer>
         </>
-            );
+    );
             
+};
+
+function ExpensesOverview() {
+    return (
+        <BottomTab.Navigator>
+            <BottomTab.Screen component={AllExpenses} name="AllExpenses" />
+            <BottomTab.Screen component={RecentExpenses} name="RecentExpenses"/>
+        </BottomTab.Navigator>
+    );
 };
